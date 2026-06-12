e78cf446392ef2bb97dfaacf689af9a887ec63c2
Farrukh <ffarrux386@gmail.com>
2026-06-12 17:16:49 +0500

    feat: Terminal section in hero

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,6 +4,7 @@ import { FeaturedProjects } from '#/components/site/FeaturedProjects'
 import Header from '#/components/site/Header'
 import Hero from '#/components/site/Hero'
 import SkillSection from '#/components/site/SkillSection'
+import Terminal from '#/components/site/Terminal'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/')({ component: Home })
@@ -17,6 +18,7 @@ function Home() {
       <SkillSection />
       <ExperienceSection />
       <FeaturedProjects />
+      <Terminal />
     </div>
   )
 }
